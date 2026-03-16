73aca335d51e3d8f4ebc9ac247926f5263c0aaed
OleksiyZaluzhnyi <al3xpr0gram1ng@gmail.com>
2026-03-16 14:35:05 +0200

    Add python files

---
+++ src/test_calculator.py
@@ -0,0 +1,11 @@
+import unittest
+from calculator import add
+
+class TestCalculator(unittest.TestCase):
+    def test_add(self):
+        # Ми кажемо: "Перевір, чи дорівнює результат додавання 2 і 3 числу 5"
+        # Якщо так - тест пройдено. Якщо ні - Jenkins видасть помилку!
+        self.assertEqual(add(2, 3), 5)
+
+if __name__ == '__main__':
+    unittest.main()
