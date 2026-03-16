e012554d6a02d65ea85f3e4f51e108df10581676
OleksiyZaluzhnyi <al3xpr0gram1ng@gmail.com>
2026-03-16 17:15:34 +0200

    fix

--- src/test_calculator.py
+++ src/test_calculator.py
@@ -5,8 +5,7 @@ class TestCalculator(unittest.TestCase):
     def test_add(self):
         # Ми кажемо: "Перевір, чи дорівнює результат додавання 2 і 3 числу 5"
         # Якщо так - тест пройдено. Якщо ні - Jenkins видасть помилку!
-        self.assertEqual(add(2, 3), 9)
-
+        self.assertEqual(add(2, 3), 5)
 
 if __name__ == '__main__':
     unittest.main()
